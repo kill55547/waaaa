012b8d2c35ab0f6d6fc32ba296d9b11ca563de38
Sora hub <79273108+kill55547@users.noreply.github.com>
2024-06-14 10:24:34 +0700

    Update Waaa.lua

--- Waaa.lua
+++ Waaa.lua
@@ -1,1 +1,4 @@
-m
+getgenv().Key = "MARU-WD8YV-GYJWZ-D385-TGW3-WLQR"
+getgenv().id = "1025586314587947098"
+getgenv().Script_Mode = "Kaitun_Script"
+loadstring(game:HttpGet("https://raw.githubusercontent.com/xshiba/MaruBitkub/main/Mobile.lua"))()
